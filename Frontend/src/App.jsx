3d6690899d1712ff60a03cf72643987ed94f9626
unknown <muhammadshahzaib12@gmail.comgit config --global user.name MuhammadShahzaib>
2025-03-22 09:47:38 +0500

    First Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-center bg-blue-300'> Hello World</h1>
+    </>
+  )
+}
+
+export default App
